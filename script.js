9aafb19a2d0332496136cffbd2520ea372f8faeb
ketki kumeriya <111123802+ketkikumeriya@users.noreply.github.com>
2025-03-17 15:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	  const charCount = {};
+  
+  for (let char of str) {
+    charCount[char] = (charCount[char] || 0) + 1;
+  }
+  
+  for (let char of str) {
+    if (charCount[char] === 1) {
+      return char;
+    }
+  }
+  
+  return null; // Return null if no non-repeated character is found
+
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
